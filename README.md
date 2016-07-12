d64b94e382924f3497119067ac5af9febd84a0a6
Naoya Murakami <visionary.incident@gmail.com>
2016-07-12 23:47:26 +0900

    Delete only broken hook

--- README.md
+++ README.md
@@ -5,7 +5,25 @@ GroongaのHookの数の確認、削除を行うコマンドを追加します
 インデックスのHookのみがカラムに残留し、カラムの削除、テーブルのtruncate、テーブルの削除ができない場合の回避策として使えるかもしれません。
 
 * hook_status
+
+```
+hook_status --table Entries --column title
+[[0,0.0,0.0],[1,[["Tokens.entries_title","(nil)"]]]]
+```
+1つ目はカラムが対象とされているインデックス名。２つ目はgrn_proc。
+通常、grn_procが設定されることはありません。
+インデックスカラムを削除してカラムにHookのみが残留している場合は、1つ目が(nil)になっています。
+
+
 * hook_delete
+
+```
+hook_delete --table Entries --column title --no_target_only true
+[[0,0.0,0.0],1]
+```
+
+デフォルトでは、ターゲットのインデックスが存在しない不正なhookのみ削除します。
+--no_target_only falseにすると１つだけ削除します。
 
 ## Install
 
--- commands/hook.c
+++ commands/hook.c
@@ -8,6 +8,11 @@
 # define GNUC_UNUSED
 #endif
 
+typedef struct {
+  grn_id target;
+  unsigned int section;
+} default_set_value_hook_data;
+
 static grn_obj *
 command_hook_delete(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **args,
                     grn_user_data *user_data)
@@ -19,6 +24,7 @@ command_hook_delete(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **a
   unsigned int table_len = 0;
   char *column_name = NULL;
   unsigned int column_len = 0;
+  grn_bool no_target_only = GRN_TRUE;
 
   var = grn_plugin_proc_get_var(ctx, user_data, "table", -1);
   if (GRN_TEXT_LEN(var) != 0) {
@@ -30,14 +36,51 @@ command_hook_delete(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **a
     column_name = GRN_TEXT_VALUE(var);
     column_len = GRN_TEXT_LEN(var);
   }
-
+  var = grn_plugin_proc_get_var(ctx, user_data, "no_target_only", -1);
+  if (GRN_TEXT_LEN(var) != 0) {
+    no_target_only = atoi(GRN_TEXT_VALUE(var));
+  }
   table = grn_ctx_get(ctx, table_name, table_len);
   column = grn_obj_column(ctx, table, column_name, column_len);
+  if (!column) {
+    GRN_PLUGIN_ERROR(ctx, GRN_INVALID_ARGUMENT,
+                     "[hook_delete] missing column");
+    return NULL;
+  }
 
   nhooks = grn_obj_get_nhooks(ctx, column, GRN_HOOK_SET);
 
   if (nhooks > 0) {
-    grn_obj_delete_hook(ctx, column, GRN_HOOK_SET, 0);
+    if (!no_target_only) {
+      grn_obj_delete_hook(ctx, column, GRN_HOOK_SET, 0);
+    } else {
+      grn_obj *hook;
+      unsigned int i;
+      grn_obj buf;
+      grn_obj data;
+      GRN_TEXT_INIT(&buf, 0);
+      GRN_TEXT_INIT(&data, 0);
+      for (i = 0; i < nhooks; i++) {
+        default_set_value_hook_data *hdata;
+        grn_obj *target;
+        GRN_BULK_REWIND(&buf);
+        GRN_BULK_REWIND(&data);
+        hook = grn_obj_get_hook(ctx, column, GRN_HOOK_SET, i, &data);
+        hdata = (void *)GRN_TEXT_VALUE(&data);
+        target = grn_ctx_at(ctx, hdata->target);
+        grn_inspect_name(ctx, &buf, target);
+        if (GRN_TEXT_LEN(&buf) > 5 &&
+            !memcmp(GRN_TEXT_VALUE(&buf), "(nil)", 5)) {
+          grn_obj_delete_hook(ctx, column, GRN_HOOK_SET, i);
+        }
+
+        GRN_BULK_REWIND(&buf);
+        grn_inspect_name(ctx, &buf, hook);
+      }
+      GRN_OBJ_FIN(ctx, &buf);
+      GRN_OBJ_FIN(ctx, &data);
+
+    }
   }
 
   nhooks = grn_obj_get_nhooks(ctx, column, GRN_HOOK_SET);
@@ -72,6 +115,11 @@ command_hook_status(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **a
 
   table = grn_ctx_get(ctx, table_name, table_len);
   column = grn_obj_column(ctx, table, column_name, column_len);
+  if (!column) {
+    GRN_PLUGIN_ERROR(ctx, GRN_INVALID_ARGUMENT,
+                     "[hook_status] missing column");
+    return NULL;
+  }
 
   nhooks = grn_obj_get_nhooks(ctx, column, GRN_HOOK_SET);
 
@@ -83,16 +131,27 @@ command_hook_status(grn_ctx *ctx, GNUC_UNUSED int nargs, GNUC_UNUSED grn_obj **a
   if (nhooks) {
     grn_obj *hook;
     unsigned int i;
-    grn_obj data;
+
     grn_obj buf;
+    grn_obj data;
     GRN_TEXT_INIT(&buf, 0);
     GRN_TEXT_INIT(&data, 0);
     for (i = 0; i < nhooks; i++) {
+      default_set_value_hook_data *hdata;
+      grn_obj *target;
       GRN_BULK_REWIND(&buf);
       GRN_BULK_REWIND(&data);
       hook = grn_obj_get_hook(ctx, column, GRN_HOOK_SET, i, &data);
+      hdata = (void *)GRN_TEXT_VALUE(&data);
+      target = grn_ctx_at(ctx, hdata->target);
+
+      grn_ctx_output_array_open(ctx, "DATA", 2);
+      grn_inspect_name(ctx, &buf, target);
+      grn_ctx_output_str(ctx, GRN_TEXT_VALUE(&buf), GRN_TEXT_LEN(&buf));
+      GRN_BULK_REWIND(&buf);
       grn_inspect_name(ctx, &buf, hook);
       grn_ctx_output_str(ctx, GRN_TEXT_VALUE(&buf), GRN_TEXT_LEN(&buf));
+      grn_ctx_output_array_close(ctx);
     }
     GRN_OBJ_FIN(ctx, &buf);
     GRN_OBJ_FIN(ctx, &data);
@@ -112,11 +171,12 @@ GRN_PLUGIN_INIT(GNUC_UNUSED grn_ctx *ctx)
 grn_rc
 GRN_PLUGIN_REGISTER(grn_ctx *ctx)
 {
-  grn_expr_var vars[2];
+  grn_expr_var vars[3];
 
   grn_plugin_expr_var_init(ctx, &vars[0], "table", -1);
   grn_plugin_expr_var_init(ctx, &vars[1], "column", -1);
-  grn_plugin_command_create(ctx, "hook_delete", -1, command_hook_delete, 2, vars);
+  grn_plugin_expr_var_init(ctx, &vars[2], "no_target_only", -1);
+  grn_plugin_command_create(ctx, "hook_delete", -1, command_hook_delete, 3, vars);
 
   grn_plugin_expr_var_init(ctx, &vars[0], "table", -1);
   grn_plugin_expr_var_init(ctx, &vars[1], "column", -1);
